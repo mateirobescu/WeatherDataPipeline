6472d4b5dac1f3516ec5761401a99fa4fd4fa91f
robescumatei <matei.robescu@gmail.com>
2025-11-29 15:48:48 +0200

    Refactor lambda_handler to improve error handling and normalize city names

--- lambda_functions/fetch_weather/lambda_function.py
+++ lambda_functions/fetch_weather/lambda_function.py
@@ -1,8 +1,9 @@
 import json
-import boto3
-from botocore.exceptions import ClientError
+import unicodedata
 import requests
 import datetime
+import boto3
+from botocore.exceptions import ClientError
 
 def get_api_secret(secret_name, region_name):
 	session = boto3.session.Session()
@@ -26,34 +27,46 @@ def get_api_secret(secret_name, region_name):
 def lambda_handler(event, context):
 	secret_name = "OpenWeatherApi"
 	region_name = "eu-north-1"
-	
 	KEY = get_api_secret(secret_name, region_name)
+	
 	city_id = event["city_id"]
 	response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?id={city_id}&units=metric&appid={KEY}')
 	
-	if response.status_code == 200:
-		data = response.json()
-		city_name = data["name"]
-		
-		s3 = boto3.client('s3')
-		bucket_name = "raw-weather-data--mateirobescu"
-		
-		file_name = f"raw/{city_id}-{city_name.replace(' ', '-').encode("ascii", "ignore").decode("ascii").lower()}_{datetime.datetime.now(tz=datetime.UTC).strftime('%Y-%m-%d')}.json"
-		
-		
-		s3.put_object(
-			Bucket=bucket_name,
+	if response.status_code != 200:
+		return {
+			'statusCode': 404,
+			'body': json.dumps(f"City with id {city_id} not found!")
+		}
+	
+	data = response.json()
+	city_name = data["name"]
+	
+	s3_client = boto3.client('s3')
+	BUCKET = "raw-weather-data--mateirobescu"
+	
+	normalized_text = unicodedata.normalize("NFKD", city_name.lower().replace(' ', '-'))
+	ascii_city_name = normalized_text.encode("ascii", "ignore").decode("ascii")
+	if not ascii_city_name:
+		ascii_city_name = "unknown"
+	
+	file_name = f"raw/{city_id}-{ascii_city_name}_{datetime.datetime.now(tz=datetime.UTC).strftime('%Y-%m-%d')}.json"
+	
+	try:
+		s3_client.put_object(
+			Bucket=BUCKET,
 			Key=file_name,
 			Body=json.dumps(data, indent=2),
 			ContentType="application/json"
 		)
-		
+	except ClientError as e:
 		return {
-			'statusCode': 200,
-			'body': json.dumps("City Found")
+			'statusCode': 400,
+			'body': f"Error putting object with key({file_name}) in bucket({BUCKET}): {e}"
 		}
 	
 	return {
-		'statusCode': 404,
-		'body': json.dumps('City not found')
+		'statusCode': 200,
+		'body': json.dumps(f"City {city_id}-{city_name} found!")
 	}
+	
+	
